ed26b43ef8b13e8cdea2e73f50be0550dc6c1276
Laura Padilla <hi@lauracodes.com>
2021-12-15 10:49:20 -0500

    Add image to rich text

--- schemas/opening.js
+++ schemas/opening.js
@@ -28,7 +28,28 @@ export default {
     {
       name: "description",
       title: "Description",
-      type: "text",
+      type: "array",
+      of: [
+        { type: "block" },
+        {
+          type: "image",
+          fields: [
+            {
+              type: "text",
+              name: "alt",
+              title: "Alternative text",
+              description: `Some of your visitors cannot see images, 
+            be they blind, color-blind, low-sighted; 
+            alternative text is of great help for those 
+            people that can rely on it to have a good idea of 
+            what\'s on your page.`,
+              options: {
+                isHighlighted: true,
+              },
+            },
+          ],
+        },
+      ],
     },
     {
       name: "moves",
